6ceb9f2278a8279a71e742af40c5a8c4a2198c40
Bardziasz <wiktorrogo@gmail.com>
2026-07-16 00:56:07 +0200

    Changed README.md ; changed sfcw generation in parameters using toolbox

--- dsp/README.md
+++ dsp/README.md
@@ -1,1 +1,7 @@
 Include basic description of the program
+
+##Taski do zrobienia
+```
+- Generacja danych (dosyć problematyczne ) / znaleść dataset ; Jak się nie uda to dopiero gdy dostaniemy płytkę będzie można przetestować działanie;
+- Na wejściu filtr dolnoprzepustowy (zależy co hardware wymyśli)
+- Zrobić te etapy range migration, nawet na losowych danych
--- dsp/parameters.m
+++ dsp/parameters.m
@@ -1,20 +1,32 @@
+%% data cells (2 cells of 2x2 matrices )
+Tx_data=cell(2);
+Rx_data=cell(2);
 
-% signal parameters
-fs=[]     ;   % sampling frequency
-fmin=[]    ;   % starting frequency
-fstep=[]   ;   % SFCW step size
-step_num=[] ;  % SFCW number of steps
-T_step=[] ;   %duration of step
-T=[];  %duration of whole signal
-B=[];  % bandwidth
+
+% radar parameters
+radar.fs=50*1e6;   % sampling frequency
+radar.fmin=[]    ;   % starting frequency
+radar.fstep=[]   ;   % SFCW step size
+radar.step_num=[] ;  % SFCW number of steps
+radar.T_step=[] ;   %duration of step
+radar.T=[];  %duration of whole signal
+radar.B=[];  % bandwidth
+radar.D_x=0.05; % antennas separation x axis [m]
+radar.D_y=0.05;  % antennas separation y axis [m]
+radar.beam_w=[];  % Antenna beamwidth [rad]
+
+
+%generating SFCW signal (need phased array toolbox)
+sfcw_wave=phased.SteppedFMWaveform;
+sfcw_wave.SampleRate=radar.fs;
+sfcw_wave.FrequencyStep=4e3;
+sfcw_wave.FrequencyOffset=96e6; % center frequency is 100MHz with 8MHz bandwidth
+sfcw_wave.NumPulses=100;
+
+
 c=299792458 ; % speed of light
-Mt=[]; % matrix length in time domain 
 
-% radar physical parameters
-Mx=4;  % number of antenas in x axis (?transmiters?)
-My=4;  % number of antenas in y axis  (?receivers?)
-D_x=0.05; % antennas separation x axis [m]
-D_y=0.05;  % antennas separation y axis [m]
-beam_w=[];  % Antenna beamwidth [rad]
+
+
 
 
